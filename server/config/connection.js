df53219b75b4bba34905b49aab12d4f2defe2046
Justin Ramos <Jramos20022@gmail.com>
2026-06-19 05:54:53 -0400

    Set MongoDB database name for Render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/illuminearsDB'
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/illuminearsDB',
+  { dbName: process.env.MONGODB_DB_NAME || 'illuminearsDB' }
 );
 
 module.exports = mongoose.connection;
